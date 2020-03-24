e31c25917a1dfa5fe806b4504ee0450d3ca9d111
Simon Rowe <simon@wiremoons.com>
2020-03-24 13:53:53 +0000

    Formating clean up

--- getURL.go
+++ getURL.go
@@ -3,23 +3,33 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 // URL format:  "https://api.darksky.net/forecast/APIKEY/LATITUDE,LONGITUDE,TIME?units=ca&lang=en"
+// https://api.darksky.net/forecast/66fd639c6914180e12c355899c5ec267/51.419212,-3.291481?exclude=minutely,hourly?units=uk2
+
 const BASEURL = "https://api.darksky.net/forecast/"
 
-//https://api.darksky.net/forecast/66fd639c6914180e12c355899c5ec267/51.419212,-3.291481?units=uk2&exclude=minutely,hourly
-//https://api.darksky.net/forecast/66fd639c6914180e12c355899c5ec267/51.419212,-3.291481?exclude=minutely,hourly?units=uk2
-
-func GetURL() (URL string, err error) {
+func GetURL(coord string) (url string, err error) {
 
 	key := "66fd639c6914180e12c355899c5ec267"
-	coord := "51.419212,-3.291481"
+
+	if coord == "" {
+		coord = "51.419212,-3.291481"
+	}
 
 	// construct final url to obtain forecast
-	URL = BASEURL + key + "/" + coord + "?units=uk2&exclude=minutely,hourly"
+	url = BASEURL + key + "/" + coord + "?units=uk2&exclude=minutely,hourly"
 
-	// if debugSwith {
-	// fmt.Println("DEBUG: final url is: ", url)
-	// }
+	if debugSwitch {
+		fmt.Println("DEBUG: final url is: ", url)
+	}
 
-	return URL, nil
+	if url == "" {
+		return "", fmt.Errorf("Unable to create DarkSky URL")
+	}
+
+	return url, nil
 }
